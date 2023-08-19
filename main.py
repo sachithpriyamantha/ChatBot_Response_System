e6d403848395ed9b69b752ae016724de773f3aa9
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-08-19 11:01:15 +0530

    complete

--- main.py
+++ main.py
@@ -20,6 +20,34 @@ def message_probability(user_message, recognised_words, single_response=False, r
             break
 
 
+    if has_required_words or single_response:
+        return int(percentage*100)
+    else:
+        return 0
+    
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+#response=====================
+
+    response('Hello!', ['hello','hi','sup','hey','heyo'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('Thank you!', ['i', 'love', 'code', 'place'], required_words=['code', 'place'])
+
+    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    #print(highest_prob_list)
+
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+
+
+
 
 def get_response(user_input):
     split_message = re.split(r'\s + |[,;!.-]\s*', user_input.lower())
@@ -29,4 +57,4 @@ def get_response(user_input):
 
 #testing the response system
 while True:
-    print('Bot: ' + get_response(input('you')))
+    print('Bot: ' + get_response(input('You : ')))
